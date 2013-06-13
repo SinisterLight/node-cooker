a69253357b998a27d63fba38f0ee1e37aaac084f
Sinister Light <me@SinisterLight.com>
2013-06-13 19:13:18 +0530

    [Shobhit] fixing enviorment setup defention and optional forever install

--- definitions/enviorment.rb
+++ definitions/enviorment.rb
@@ -2,13 +2,14 @@ define :enviorment_setup do
   application_name = params[:name]
 
   template "/etc/profile.d/#{params[:name]}.sh" do
+    cookbook params[:cookbook]
     source params[:source]
     mode 00644
     owner 'root'
-    notifies :restart, "service[jboss-as-standalone]"
+    notifies :restart, "execute[restart forever]"
   end
 
-  execute "restart" do
+  execute "restart forever" do
     Chef::Log.info("Installing restarting app")
     command "forever restartall"
     not_if do
--- recipes/forever.rb
+++ recipes/forever.rb
@@ -1,3 +1,6 @@
 node_package "forever" do
   action :install
+  not_if do
+    File.exists?("/usr/local/bin/forever")
+  end
 end
